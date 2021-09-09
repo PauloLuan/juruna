bc1aebda721e59828841ce50782b678b04e206e4
Paulo Luan <pauloluan.inova@gmail.com>
2021-09-09 05:45:05 -0300

    🎉 fix: first version of command line args

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,45 @@
-export * from './cli'
+#!/usr/bin/env node
+import { string } from '@jest/types/node_modules/@types/yargs'
+import yargs from 'yargs'
+
+const generators = {
+  rc: {
+    description: 'rc: React component',
+    generatorFn: () => {} //require('./cli')
+  },
+  rp: {
+    description: 'rp: React Project',
+    generatorFn: () => {} //require('./cli')
+  }
+}
+
+const choices: string[] = Object.keys(generators)
+const descriptions = choices
+  .map((choice) => {
+    const choiceDescription = generators[choice].description + '\n'
+    return choiceDescription
+  })
+  .join()
+
+const argv = yargs.options({
+  type: {
+    choices,
+    demandOption: true,
+    alias: 't',
+    description: descriptions
+  },
+  name: {
+    type: 'string',
+    demandOption: true,
+    alias: 'n',
+    description: 'the name of generator component or project'
+  }
+}).argv
+
+const { type, name }: any = argv
+
+console.log('==============')
+console.log('argv :>> ', JSON.stringify({ type, name }, null, 2))
+console.log('==============')
+
+const selectedGenerator = generators[type]
